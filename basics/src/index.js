f46ab0d0ab740d3d1a78c1c1aca0fc878ab8ba6e
Dinar <maildinuju@gmai.com>
2020-10-22 23:06:15 +0700

    added nodmon and some nested query

--- basics/src/index.js
+++ basics/src/index.js
@@ -3,13 +3,26 @@ import { GraphQLServer } from 'graphql-yoga'
 const typeDefs = `
     type Query {
         hello:String!
+        title:String
+        balance: Int!
+        owe: Float
+        books: [Book!]
+    }
+
+    type Book {
+        name: String!
     }
 `
 
 const resolvers = {
     Query: {
-        hello: _ => "Hello world"
+        hello: _ => "Hello world",
+        title: _ => null,
+        balance: _ => 500,
+        owe: _ => 50.5,
+        books: _ => [{ name: "art of war" }]
     }
+
 }
 
 const server = new GraphQLServer({ typeDefs, resolvers });
