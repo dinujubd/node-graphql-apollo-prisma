30f4d806a54c08977a51ee0bc169dc1d2ec47d3a
Dinar <maildinuju@gmai.com>
2020-10-23 11:39:16 +0700

    moved and added pubsub

--- basics/src/index.js
+++ basics/src/index.js
@@ -1,10 +1,12 @@
-import { GraphQLServer } from 'graphql-yoga'
-import Query from './query'
-import Mutation from './mutation'
-import Book from './book'
-import Author from './author'
+import { GraphQLServer, PubSub } from 'graphql-yoga'
+import Query from './resolvers/query'
+import Mutation from './resolvers/mutation'
+import Book from './resolvers/book'
+import Author from './resolvers/author'
+import Subscription from './resolvers/subscription'
 import db from './db'
 
-const server = new GraphQLServer({ typeDefs: "./src/schema.graphql", resolvers: { Query, Mutation, Book, Author }, context: { db } });
+const pubsub = new PubSub()
+const server = new GraphQLServer({ typeDefs: "./src/schema.graphql", resolvers: { Query, Mutation, Subscription, Book, Author }, context: { db, pubsub } });
 
 server.start(() => console.log('Server is running on localhost:4000'))
